b702aee610093c5a8253ab93a73adedec01c86c2
Christian <5768801+christianjuth@users.noreply.github.com>
2025-09-06 21:17:53 -0400

    refactor: update dataset

--- cralwer.ts
+++ cralwer.ts
@@ -1,13 +1,8 @@
 import z from 'zod'
 import _ from 'lodash'
-import chalk from 'chalk';
 import { dateLessThanOneMonthAgo } from './utils/date';
 import { BasicCrawler, Dataset, RequestQueue } from "crawlee";
-import { BaseHttpClient, Log } from "@crawlee/core";
 import fs from 'node:fs/promises'
-
-const VERBOSE_LEVEL = 0;
-const KNOWN = new Set<string>;
 
 function isSupportedSoftware(software: string) {
   switch (software.trim().toLowerCase()) {
@@ -16,12 +11,6 @@ function isSupportedSoftware(software: string) {
       return true;
     default:
       return false;
-  }
-}
-
-function verboseLog(level: number, ...text: string[]) {
-  if (VERBOSE_LEVEL >= level) {
-    console.log(chalk.gray(text.join(" ")));
   }
 }
 
@@ -71,37 +60,35 @@ const federatedInstancesSchema = z.object({
   })
 })
 
-type Instance = {
-  url: string,
-  host: string,
-  description?: string,
-  icon?: string,
-  software: "lemmy" | "piefed"
-  registrationMode: string,
-};
+// type Instance = {
+//   url: string,
+//   host: string,
+//   description?: string,
+//   icon?: string,
+//   software: "lemmy" | "piefed"
+//   registrationMode: string,
+// };
 
 async function crawl() {
   const dataset = await Dataset.open();
 
   const requestQueue = await RequestQueue.open();
 
-  const processQueueItem = async (
-    instance: string,
-    sendRequest: BaseHttpClient['sendRequest'],
-    log: Log
-  ) => {
-    KNOWN.add(instance);
+  const crawler = new BasicCrawler({
+    requestQueue,
+    maxConcurrency: 50,
+    minConcurrency: 5,
+    requestHandlerTimeoutSecs: 10_000,
+    requestHandler: async ({ request, log, sendRequest }) => {
+      async function get<S extends z.ZodObject>(url: string, schema: S) {
+        const nodeInfoReq = await sendRequest({
+          url,
+          method: "GET"
+        })
+        return schema.parse(JSON.parse(nodeInfoReq.body))
+      }
 
-    async function get<S extends z.ZodObject>(url: string, schema: S) {
-      const nodeInfoReq = await sendRequest({
-        url,
-        method: "GET"
-      })
-      return schema.parse(JSON.parse(nodeInfoReq.body))
-    }
-
-    try {
-      instance = normalizeInstance(instance);
+      const instance = normalizeInstance(request.url);
 
       const host = new URL(instance).host;
 
@@ -114,7 +101,6 @@ async function crawl() {
         for (const linked of res.federated_instances.linked) {
           const updated = linked.updated ?? linked.published;
           if (linked.software && isSupportedSoftware(linked.software) && updated && dateLessThanOneMonthAgo(updated)) {
-            verboseLog(2, "Found: ", linked.domain)
             requestQueue.addRequests([normalizeInstance(linked.domain)])
           }
         }
@@ -155,18 +141,6 @@ async function crawl() {
           break;
         }
       }
-    } catch { }
-  };
-
-  const crawler = new BasicCrawler({
-    requestQueue,
-    maxConcurrency: 50,
-    minConcurrency: 5,
-    requestHandlerTimeoutSecs: 10_000,
-    // limit per host:
-    // use RequestQueue with same hostname keys or run multiple crawlers per host
-    requestHandler: async ({ request, log, sendRequest }) => {
-      await processQueueItem(request.url, sendRequest, log)
     },
   });
 
@@ -186,6 +160,8 @@ async function crawl() {
   await crawler.run([
     "https://lemmy.world",
     "https://lemmy.zip",
+    "https://lemmy.ml",
+    "https://piefed.world",
   ]);
 
   clearTimeout(id1)
